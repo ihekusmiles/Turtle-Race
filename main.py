b0dda59a189e39f4b934e329d719f079ac0fa59e
Hector <168874717+ihekusmiles@users.noreply.github.com>
2024-06-03 21:50:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -35,9 +35,9 @@ while is_race_on:
             is_race_on = False
             winning_color = turtle.pencolor()
             if winning_color == user_bet:
-                print(f"You've won! The {winning_color} is the winner!")
+                print(f"You've won! The {winning_color} turtle is the winner!")
             else:
-                print(f"You've lost. The {winning_color} is the winner!")
+                print(f"You've lost. The {winning_color} turtle is the winner!")
 
 screen.exitonclick()
 
